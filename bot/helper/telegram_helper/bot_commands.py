ed9601a7b2e74ece0a2841693b63aecb87abaf20
Vinayak703 <76476634+vinayakext2@users.noreply.github.com>
2021-04-05 19:35:03 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -1,6 +1,6 @@
 class _BotCommands:
     def __init__(self):
-        self.StartCommand = 'start@ziggyuplaoder2bot'
+        self.StartCommand = 'start'
         self.MirrorCommand = 'mirror'
         self.UnzipMirrorCommand = 'unzipmirror'
         self.TarMirrorCommand = 'tarmirror'
